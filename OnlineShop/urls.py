5e44e10bff6eed4bde90b1de27275fe7804d0b36
kingaliubunto <alimardalishahi8013@gmail.com>
2023-04-14 20:00:39 +0330

    rewrite code to the  clean code

--- OnlineShop/urls.py
+++ OnlineShop/urls.py
@@ -14,13 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path,include
-from django.conf import  settings
+from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('home.urls',namespace='home')),
-    path('accounts/', include('accounts.urls', namespace='accounts')),
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('', include('home.urls', namespace='home')),
+                  path('accounts/', include('accounts.urls', namespace='accounts')),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

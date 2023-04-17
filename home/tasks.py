6dd7e6674acbf21fb894fc3f668a811880ae31a2
kingaliubunto <alimardalishahi8013@gmail.com>
2023-04-17 08:16:38 +0330

    add deleet and downlaod for bucket

--- home/tasks.py
+++ home/tasks.py
@@ -1,14 +1,18 @@
-from bucket import bucket
 from celery import shared_task
+
+from bucket import bucket
+
 
 # TODO: can be asyne
 def all_bucket_object_task():
     result = bucket.get_objects()
     return result
 
+
 @shared_task
 def delete_object_bucket(key):
     bucket.delete_bucket_object(key)
+
 
 @shared_task
 def download_bucket_object(key):
--- home/urls.py
+++ home/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
+
 from . import views
 
-app_name='home'
+app_name = 'home'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('<slug:slug>', views.ProductDetailView.as_view(), name='product_detail'),
-    path('bucket/',views.BucketHome.as_view(),name='bucket'),
-    path('delete_obj_bucket/<key>',views.DeleteBucketObject.as_view(),name='delete_obj_bucket'),
-    path('download_obj_bucket/<key>',views.DownloadBucketObject.as_view(),name='download_obj_bucket'),
+    path('bucket/', views.BucketHome.as_view(), name='bucket'),
+    path('delete_obj_bucket/<key>', views.DeleteBucketObject.as_view(), name='delete_obj_bucket'),
+    path('download_obj_bucket/<key>', views.DownloadBucketObject.as_view(), name='download_obj_bucket'),
 
 ]

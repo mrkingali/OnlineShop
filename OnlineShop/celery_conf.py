a25b2c2ff244d0a83d163a600d43a72d216c24bd
kingaliubunto <alimardalishahi8013@gmail.com>
2023-04-18 18:54:23 +0330

    some clean code

--- OnlineShop/celery_conf.py
+++ OnlineShop/celery_conf.py
@@ -1,18 +1,18 @@
-from celery import Celery
+import os
 from datetime import timedelta
-import  os
 
+from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE','OnlineShop.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OnlineShop.settings')
 
-celery_app =Celery('OnlineShop')
+celery_app = Celery('OnlineShop')
 celery_app.autodiscover_tasks()
 
-celery_app.conf.broker_url='amqp://'
-celery_app.conf.result_backend='rpc://'
-celery_app.conf.task_serializer='json'
-celery_app.conf.result_serializer='pickle'
-celery_app.conf.accept_content=['json','pickle']
-celery_app.result_expires=timedelta(days=1)
-celery_app.task_always_eager=False
-celery_app.worker_prefetch_multiplier=4
+celery_app.conf.broker_url = 'amqp://'
+celery_app.conf.result_backend = 'rpc://'
+celery_app.conf.task_serializer = 'json'
+celery_app.conf.result_serializer = 'pickle'
+celery_app.conf.accept_content = ['json', 'pickle']
+celery_app.result_expires = timedelta(days=1)
+celery_app.task_always_eager = False
+celery_app.worker_prefetch_multiplier = 4
--- OnlineShop/settings.py
+++ OnlineShop/settings.py
@@ -114,13 +114,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[
-    BASE_DIR/'static'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
 ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -136,4 +134,4 @@ AWS_S3_ENDPOINT_URL = 'https://s3.ir-thr-at1.arvanstorage.com/'
 AWS_STORAGE_BUCKET_NAME = 'django-online-shop-project'
 AWS_SERVICE_NAME = 's3'
 AWS_S3_FILE_OVERWRITE = False
-AWS_LOCAL_STORAGES=f'{BASE_DIR}/aws/'
+AWS_LOCAL_STORAGES = f'{BASE_DIR}/aws/'

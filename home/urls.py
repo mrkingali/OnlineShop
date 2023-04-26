1fce13278b33d9f3ef996421c334bbccf0d3bb09
kingaliubunto <alimardalishahi8013@gmail.com>
2023-04-26 12:30:04 +0330

    set category in front and backend

--- home/urls.py
+++ home/urls.py
@@ -1,13 +1,19 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 app_name = 'home'
+
+bucket_urls = [
+    path('', views.BucketHome.as_view(), name='bucket'),
+    path('delete_obj_bucket/<str:key>', views.DeleteBucketObject.as_view(), name='delete_obj_bucket'),
+    path('download_obj_bucket/<str:key>', views.DownloadBucketObject.as_view(), name='download_obj_bucket'),
+]
+
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('<slug:slug>', views.ProductDetailView.as_view(), name='product_detail'),
-    path('bucket/', views.BucketHome.as_view(), name='bucket'),
-    path('delete_obj_bucket/<key>', views.DeleteBucketObject.as_view(), name='delete_obj_bucket'),
-    path('download_obj_bucket/<key>', views.DownloadBucketObject.as_view(), name='download_obj_bucket'),
+    path('bucket/', include(bucket_urls)),
+    path('category/<slug:category_slug>/', views.HomeView.as_view(), name='category_filter')
 
 ]
--- home/views.py
+++ home/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render, get_object_or_404,redirect
 from django.views import View
-from .models import Product
+from .models import Product,Category
 from . import tasks
 from django.contrib import messages
 from utils import IsAdminUserMixin
 class HomeView(View):
 
-    def get(self, request):
+    def get(self, request,category_slug=None):
         products = Product.objects.filter(available=True)
-        return render(request, 'home/home.html', {'products': products})
+        categories=Category.objects.filter()
+        if category_slug:
+            category=categories.get(slug=category_slug)
+            products=products.filter(category=category)
+        return render(request, 'home/home.html', {'products': products,'categories':categories})
 
 
 

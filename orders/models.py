6f801a9677260a181d7719d59aa50e1a704c0d08
kingaliubunto <alimardalishahi8013@gmail.com>
2023-05-09 10:22:27 +0330

    clean code

--- orders/models.py
+++ orders/models.py
@@ -1,16 +1,17 @@
-from django.db import models
-from home.models import Product
 from django.contrib.auth import get_user_model
+from django.db import models
+
+from home.models import Product
 
 
 class Order(models.Model):
-    user=models.ForeignKey(get_user_model(),on_delete=models.CASCADE,related_name='orders')
-    paid=models.BooleanField(default=False)
-    created=models.DateTimeField(auto_now_add=True)
-    updated=models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='orders')
+    paid = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering=('paid','-updated')
+        ordering = ('paid', '-updated')
 
     def __str__(self):
         return f'{self.user} {self.id}'
@@ -18,13 +19,15 @@ class Order(models.Model):
     def get_total_price(self):
         return sum(item.get_cost() for item in self.items.all())
 
+
 class OrderItems(models.Model):
-    orders=models.ForeignKey(Order,on_delete=models.CASCADE,related_name='items')
-    product=models.ForeignKey(Product,on_delete=models.CASCADE)
-    price=models.IntegerField()
-    quantity=models.IntegerField(default=1)
+    orders = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.IntegerField()
+    quantity = models.IntegerField(default=1)
 
     def __str__(self):
         return self.id
+
     def get_cost(self):
-        return self.price*self.quantity
+        return self.price * self.quantity
